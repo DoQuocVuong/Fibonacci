4be2d17d1e509dabfc0f4f00728b673c267dd5d2
DoQuocVuong <49827483+DoQuocVuong@users.noreply.github.com>
2019-07-23 21:53:38 +0700

    không sử dụng đệ quy

--- Fibonacci.cs
+++ Fibonacci.cs
@@ -5,42 +5,55 @@ namespace ConsoleApplication1
 {
     public class Fibonacci
     {
-        public static int fibonacci(int n) {
-            if (n < 0) {
-                return -1;
-            } else if (n == 0 || n == 1) {
-                return n;
-            } else {
-                return fibonacci(n - 1) + fibonacci(n - 2);
-            }
-        }
-//        public static int tim(int n)
-//        {
-//             return tim(n-1)+ tim(n-2);
+//        public static int fibonacci(int n) {
+//            if (n < 0) {
+//                return -1;
+//            } else if (n == 0 || n == 1) {
+//                return n;
+//            } else {
+//                return fibonacci(n - 1) + fibonacci(n - 2);
+//            }
 //        }
+        public static int tim(int n)
+        {
+            int a1 = 1;
+            int a2 = 2;
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            int i = 3;
+            int a;
+            while (i <= n)
+            {
+                a = a1 + a2;
+                a1 = a2;
+                a2 = a;
+                i++;
+                
+            }
+
+            return a1 + a2;
+        }
+
         public static void Main(string[] urg)
         {
-            Console.WriteLine("Input number:");
-            int x = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < x; i++) {
-                           x += fibonacci(i);
-                           Console.WriteLine(fibonacci(i) + " ");
-            }
-               
-            Console.WriteLine("Tổng: " + x);
-                   }
-//            Console.WriteLine("Input number: ");
-//            int x1 = 0;
-//            int x2 = 1;
-//            int x3 = 1;
+//            Console.WriteLine("Input number:");
 //            int x = Convert.ToInt32(Console.ReadLine());
-//            for (int i = 0; i < x; i++)
-//            {
-//                x1 = x2;
-//                x2 = x3;
-//                x3 = tim(i);
-            }
-            
+//            for (int i = 0; i < x; i++) {
+//                           x += fibonacci(i);
+//                           Console.WriteLine(fibonacci(i) + " ");
+//            }
+//               
+//            Console.WriteLine("Tổng: " + x);
+//                   }
+            Console.WriteLine("Input number: ");
+            int x1 = 0;
+            int x2 = 1;
+            int x3 = 1;
+            int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Số cần tìm là:"+ tim(n));
         }
-//    }
-//}
+    }
+}
